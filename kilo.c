91387ba220889fb91b65b00e76b2d827bd289b4f
Matthew Vogee <mvogee@yahoo.com>
2020-10-26 21:37:50 -0700

    progress

--- kilo.c
+++ kilo.c
@@ -1,19 +1,29 @@
+/*** includes ***/
+
 #include <ctype.h>
 #include <stdio.h>
+#include <errno.h>
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
 
-
+/*** data ***/
 struct termios orig_termios;
 
+
+/*** terminal ***/
+void die(const char *s) {
+  perror(s);
+  exit(1);
+}
+
 void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
 }
 
 void enableRawMode() {
-
-  tcgetattr(STDIN_FILENO, &orig_termios);
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
@@ -24,14 +34,16 @@ void enableRawMode() {
   raw.c_cc[VMIN] = 0;
   raw.c_cc[VTIME] = 1;
 
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
+
+/*** init ***/
 
 int main() {
   enableRawMode();
   while (1) {
     char c = '\0';
-    read(STDIN_FILENO, &c, 1);
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
     if (iscntrl(c)) {
       printf("%d ('%c')\n", c, c);
     }
